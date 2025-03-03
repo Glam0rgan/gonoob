880649e43382d79c37b6552d322732b72a00aa1f
Glam0rgan <1742771620@qq.com>
2025-03-03 13:44:21 +0800

    add connect

--- logic/publish.go
+++ logic/publish.go
@@ -3,10 +3,74 @@ package logic
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"strings"
+	"time"
+
+	"github.com/rcrowley/go-metrics"
 )
 
 var RedisClient *redis.Client
 var RedisSessClient *redis.Client
+
+func (logic *Logic) InitPublishRedisClient() (err error) {
+	redisOpt := tools.RedisOption{
+		Address:  config.Conf.Common.CommonRedis.RedisAddress,
+		Password: config.Conf.Common.CommonRedis.RedisPassword,
+		Db:       config.Conf.Common.CommonRedis.Db,
+	}
+	RedisClient = tools.GetRedisInstance(redisOpt)
+	if pong, err := RedisClient.Ping().Result(); err != nil {
+		logrus.Infof("RedisCli Ping Result pong: %s,  err: %s", pong, err)
+	}
+	//this can change use another redis save session data
+	RedisSessClient = RedisClient
+	return err
+}
+
+func (logic *Logic) InitRpcServer() (err error) {
+	var network, addr string
+	// a host multi port case
+	rpcAddressList := strings.Split(config.Conf.Logic.LogicBase.RpcAddress, ",")
+	for _, bind := range rpcAddressList {
+		if network, addr, err = tools.ParseNetwork(bind); err != nil {
+			logrus.Panicf("InitLogicRpc ParseNetwork error : %s", err.Error())
+		}
+		logrus.Infof("logic start run at-->%s:%s", network, addr)
+		go logic.createRpcServer(network, addr)
+	}
+	return
+}
+
+func (logic *Logic) createRpcServer(network string, addr string) {
+	s := server.NewServer()
+	logic.addRegistryPlugin(s, network, addr)
+	// serverId must be unique
+	//err := s.RegisterName(config.Conf.Common.CommonEtcd.ServerPathLogic, new(RpcLogic), fmt.Sprintf("%s", config.Conf.Logic.LogicBase.ServerId))
+	err := s.RegisterName(config.Conf.Common.CommonEtcd.ServerPathLogic, new(RpcLogic), fmt.Sprintf("%s", logic.ServerId))
+	if err != nil {
+		logrus.Errorf("register error:%s", err.Error())
+	}
+	s.RegisterOnShutdown(func(s *server.Server) {
+		s.UnregisterAll()
+	})
+	s.Serve(network, addr)
+}
+
+func (logic *Logic) addRegistryPlugin(s *server.Server, network string, addr string) {
+	r := &serverplugin.EtcdV3RegisterPlugin{
+		ServiceAddress: network + "@" + addr,
+		EtcdServers:    []string{config.Conf.Common.CommonEtcd.Host},
+		BasePath:       config.Conf.Common.CommonEtcd.BasePath,
+		Metrics:        metrics.NewRegistry(),
+		UpdateInterval: time.Minute,
+	}
+	err := r.Start()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	s.Plugins.Add(r)
+}
 
 func (logic *Logic) RedisPublishChannel(serverId string, toUserId int, msg []byte) (err error) {
 	redisMsg := proto.RedisMsg{
